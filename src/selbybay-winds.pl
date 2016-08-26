c56f70236327283a44e59681c2bb3e3b5676b1d9
Christiam Camacho <christiam@hotmail.com>
2016-08-26 08:41:48 -0400

    Fix source of forecast

--- src/selbybay-winds.pl
+++ src/selbybay-winds.pl
@@ -7,14 +7,21 @@ use Readonly;
 use autodie;
 use HTML::Strip;
 
-Readonly my $NOAA_SITE => 'http://weather.noaa.gov/cgi-bin/fmtbltn.pl?file=forecasts/marine/coastal/an/anz532.txt';
+Readonly my $NOAA_SITE => 'http://www.ndbc.noaa.gov/station_page.php?station=tplm2';
 Readonly my $LOCATION => "SANDY POINT TO NORTH BEACH";
 
 my $forecast;
 my $browser = WWW::Mechanize->new(autocheck=>1);
 $browser->get($NOAA_SITE);
-my $hs = HTML::Strip->new();
-$forecast = $hs->parse($browser->response->decoded_content);
+$browser->follow_link(text_regex=>qr/Latest NWS Marine Forecast/i);
+my @records = split(qr(</pre><hr size="1"/><pre>\n), $browser->response->decoded_content);
+foreach (@records) {
+    if (/$LOCATION/i) {
+        my $hs = HTML::Strip->new();
+        $forecast = $hs->parse($_);
+        last;
+    }
+}
 
 if (@ARGV) {
     my $email = MIME::Lite->new(
